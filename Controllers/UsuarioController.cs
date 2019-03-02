894e9b4adff6f92e57b63dd1c99090ace0437eb8
Eduardo Alcantara de Oliveira~ <eoalcantara@gmail.com>
2019-03-02 17:21:23 -0300

    CRUD 01 - Exemplo usuario - OK

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -1,19 +1,62 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BootStrapDDDWebMvc.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
 
+//Todo esta uma zona refatorar 
 namespace BootStrapDDDWebMvc.Controllers
 {
     public class UsuarioController : Controller
     {
+        private static readonly HttpClient client = new HttpClient();
+
+
         // GET: Usuario
         public ActionResult Index()
         {
+            using (var api = new HttpClient())
+            {
+                api.BaseAddress = new Uri("https://localhost:44332/api/");
+
+                var get = api.GetAsync("usuario");
+                get.Wait();
+
+                var retorno = get.Result;
+
+                if (retorno.IsSuccessStatusCode)
+                {                    
+                    var readTask = retorno.Content.ReadAsAsync<IEnumerable<Usuario>>().Result;
+
+                    return View(readTask);
+                }
+            }
+
             return View();
         }
 
         // GET: Usuario/Details/5
         public ActionResult Details(int id)
         {
+            string url = "https://localhost:44332/api/usuario";
+            using (var api = new HttpClient())
+            {
+                api.BaseAddress = new Uri("https://localhost:44332/api/");
+                
+                var get = api.GetAsync(url + "/" + id.ToString());
+                get.Wait();
+
+                var retorno = get.Result;
+
+                if (retorno.IsSuccessStatusCode)
+                {
+                    var readTask = retorno.Content.ReadAsAsync<Usuario>().Result;
+
+                    return View(readTask);
+                }
+            }
+
             return View();
         }
 
@@ -26,11 +69,27 @@ namespace BootStrapDDDWebMvc.Controllers
         // POST: Usuario/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create(Usuario collection)
         {
             try
             {
                 // TODO: Add insert logic here
+                using (var api = new HttpClient())
+                {
+                    api.BaseAddress = new Uri("https://localhost:44332/api/");
+
+
+                    var post = api.PostAsJsonAsync("usuario", (collection));
+                    post.Wait();
+
+                    var retorno = post.Result;
+
+                    if (retorno.IsSuccessStatusCode)
+                    {
+                        return RedirectToAction(nameof(Index));
+                    }
+                }
+                    
 
                 return RedirectToAction(nameof(Index));
             }
@@ -43,17 +102,52 @@ namespace BootStrapDDDWebMvc.Controllers
         // GET: Usuario/Edit/5
         public ActionResult Edit(int id)
         {
+            string url = "https://localhost:44332/api/usuario";
+            using (var api = new HttpClient())
+            {
+                api.BaseAddress = new Uri("https://localhost:44332/api/");
+
+                var get = api.GetAsync(url + "/" + id.ToString());
+                get.Wait();
+
+                var retorno = get.Result;
+
+                if (retorno.IsSuccessStatusCode)
+                {
+                    var readTask = retorno.Content.ReadAsAsync<Usuario>().Result;
+
+                    return View(readTask);
+                }
+            }
+
             return View();
         }
 
         // POST: Usuario/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(int id, Usuario collection)
         {
             try
             {
-                // TODO: Add update logic here
+                string url = "https://localhost:44332/api/usuario";
+                // TODO: Add insert logic here
+                using (var api = new HttpClient())
+                {
+                    api.BaseAddress = new Uri("https://localhost:44332/api/");
+
+
+                    var post = api.PutAsJsonAsync(url + "/" + id, (collection));
+                    post.Wait();
+
+                    var retorno = post.Result;
+
+                    if (retorno.IsSuccessStatusCode)
+                    {
+                        return RedirectToAction(nameof(Index));
+                    }
+                }
+
 
                 return RedirectToAction(nameof(Index));
             }
@@ -66,6 +160,24 @@ namespace BootStrapDDDWebMvc.Controllers
         // GET: Usuario/Delete/5
         public ActionResult Delete(int id)
         {
+            string url = "https://localhost:44332/api/usuario";
+            using (var api = new HttpClient())
+            {
+                //api.BaseAddress = new Uri("https://localhost:44332/api/");
+
+                var get = api.GetAsync(url + "/" + id.ToString());
+                get.Wait();
+
+                var retorno = get.Result;
+
+                if (retorno.IsSuccessStatusCode)
+                {
+                    var readTask = retorno.Content.ReadAsAsync<Usuario>().Result;
+
+                    return View(readTask);
+                }
+            }
+
             return View();
         }
 
@@ -76,9 +188,22 @@ namespace BootStrapDDDWebMvc.Controllers
         {
             try
             {
-                // TODO: Add delete logic here
+                string url = "https://localhost:44332/api/usuario";
+                using (var api = new HttpClient())
+                {                  
 
-                return RedirectToAction(nameof(Index));
+                    var get = api.DeleteAsync(url + "/" + id.ToString());
+                    get.Wait();
+
+                    var retorno = get.Result;
+
+                    if (retorno.IsSuccessStatusCode)
+                    {
+                        return RedirectToAction(nameof(Index));
+                    }
+                }
+
+                return View();
             }
             catch
             {
