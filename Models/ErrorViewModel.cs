61b1f9d639757245bc8907041acc810af7c9e122
Eduardo Alcantara de Oliveira~ <eoalcantara@gmail.com>
2019-03-02 11:43:37 -0300

    Primeira subida bootstrap MVC - Acessando Web Api

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BootStrapDDDWebMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
